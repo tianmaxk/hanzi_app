53a97ff2cf9a282f2986d20b6db855a5d122558f
taomin <taomin@whptp.com>
2018-08-31 11:36:40 +0800

    提交新功能 device info

---
+++ lib/common/deviceInfo.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+import 'package:device_info/device_info.dart';
+import 'dart:async';
+
+class DeviceInfo {
+  static final DeviceInfoPlugin deviceInfoPlugin = new DeviceInfoPlugin();
+
+  static Future<Map<String, dynamic>> getDeviceInfo() async {
+    Map<String, dynamic> deviceData = <String, dynamic>{};
+    try {
+      if (Platform.isAndroid) {
+        deviceData = readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on Exception {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+    return deviceData;
+  }
+
+  static Map<String, dynamic> readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+    };
+  }
+
+  static Map<String, dynamic> readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'mainPage.dart';
 import 'page/findHanzi.dart';
+import 'common/deviceInfo.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+
+  _getDeviceInfo() async {
+    Map<String, dynamic> devinfo = await DeviceInfo.getDeviceInfo();
+    print('devinfo=$devinfo');
+  }
+
   @override
   Widget build(BuildContext context) {
+    _getDeviceInfo();
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
